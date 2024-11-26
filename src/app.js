238af3c9cb896a08d3e21b2067309179f550ccb9
OussamaOR <mohamed.oussama.ould.rouis@ensia.edu.dz>
2024-11-26 18:23:05 +0100

    Minor Changes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const userRoutes = require('./routes/UserRoutes');
 const productRoutes = require('./routes/ProductRoutes');
+const customerRoutes = require('./routes/CustomerRoutes');
+const supplierRoutes = require('./routes/SupplierRoutes');
+
 
 // const routes = require('./routes');
 // const { errorHandler } = require('./middlewares/errorHandler');
@@ -41,10 +44,11 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
-
-
+app.use('/customers', customerRoutes);
+app.use('/suppliers', supplierRoutes);
 // API Routes
 // app.use('/api', routes);
+
 
 // Error Handling
 // app.use(errorHandler);

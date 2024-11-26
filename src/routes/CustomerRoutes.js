e7f7ca0d66b767dfe1024fcda65301e697d0c2cf
OussamaOR <mohamed.oussama.ould.rouis@ensia.edu.dz>
2024-11-26 17:24:26 +0100

    Added Customer Routes

--- src/routes/CustomerRoutes.js
+++ src/routes/CustomerRoutes.js
@@ -1,11 +1,134 @@
 const express = require('express');
-const { getCustomers, getCustomer, addCustomer, updateCustomer, deleteCustomer } = require('../controllers/customerController');
 const router = express.Router();
+const customerController = require('../controllers/CustomerController');
 
-router.get('/', getCustomers);
-router.get('/:customer_id', getCustomer);
-router.post('/', addCustomer);
-router.put('/:customer_id', updateCustomer);
-router.delete('/:customer_id', deleteCustomer);
+/**
+ * @swagger
+ * /customers:
+ *   post:
+ *     summary: Create a new customer
+ *     tags: [Customer]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               phone:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Customer created successfully
+ *       500:
+ *         description: Internal server error
+ */
+router.post('/', customerController.createCustomer);
+
+/**
+ * @swagger
+ * /customers:
+ *   get:
+ *     summary: Get all customers
+ *     tags: [Customer]
+ *     responses:
+ *       200:
+ *         description: A list of customers
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/', customerController.getAllCustomers);
+
+/**
+ * @swagger
+ * /customers/{id}:
+ *   get:
+ *     summary: Get a customer by ID
+ *     tags: [Customer]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The customer ID
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A customer object
+ *       404:
+ *         description: Customer not found
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/:id', customerController.getCustomerById);
+
+/**
+ * @swagger
+ * /customers/{id}:
+ *   put:
+ *     summary: Update a customer by ID
+ *     tags: [Customer]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The customer ID
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               phone:
+ *                 type: string
+ *               address:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Customer updated successfully
+ *       404:
+ *         description: Customer not found
+ *       500:
+ *         description: Internal server error
+ */
+router.put('/:id', customerController.updateCustomer);
+
+/**
+ * @swagger
+ * /customers/{id}:
+ *   delete:
+ *     summary: Delete a customer by ID
+ *     tags: [Customer]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The customer ID
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Customer deleted successfully
+ *       404:
+ *         description: Customer not found
+ *       500:
+ *         description: Internal server error
+ */
+router.delete('/:id', customerController.deleteCustomer);
 
 module.exports = router;

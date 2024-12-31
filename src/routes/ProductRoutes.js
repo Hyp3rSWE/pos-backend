6e8af7918d432ab76474d4c8424c156a297f676c
nesrine-aibot <124919116+nesrine-aibot@users.noreply.github.com>
2024-12-31 22:03:44 +0100

    modify the route

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -7,6 +7,7 @@ router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
 router.get("/barcode/:barcode", productController.getProductByBarcode);
 router.put("/:id", productController.updateProduct);
+router.put("/updateQuantity/:id", productController.updateQuantity);
 router.delete("/:id", productController.deleteProduct);
 
 module.exports = router;

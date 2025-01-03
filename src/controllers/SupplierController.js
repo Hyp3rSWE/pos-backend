70735cba5e3a050a42398cb768df1bc9760ea64b
OussamaOR <mohamed.oussama.ould.rouis@ensia.edu.dz>
2025-01-03 05:40:02 +0100

    Added linking functions

--- src/controllers/SupplierController.js
+++ src/controllers/SupplierController.js
@@ -1,4 +1,5 @@
 const Supplier = require("../models/Supplier");
+const Product = require("../models/Product");
 
 /**
  * @swagger
@@ -84,6 +85,33 @@ class SupplierController {
             }
             await supplier.destroy();
             res.status(204).json({ message: "Supplier deleted successfully" });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    // Get all products for a supplier
+    static async getSupplierProducts(req, res) {
+        try {
+            const { id } = req.params;
+            const supplier = await Supplier.findByPk(id, {
+                include: [{
+                    model: Product,
+                    attributes: [
+                        'product_id',
+                        'product_name',
+                        'product_barcode',
+                        'product_price',
+                        'product_stock_level'
+                    ]
+                }]
+            });
+
+            if (!supplier) {
+                return res.status(404).json({ message: "Supplier not found" });
+            }
+
+            res.status(200).json(supplier.Products);
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
